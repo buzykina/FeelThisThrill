08997419756f927a5f65c4d6fc7ae1cf67734947
Buzykina,Yevheniia Y.V <y.buzykina@student.fontys.nl>
2018-04-01 17:53:51 +0200

    Index page

--- Website/index.php
+++ Website/index.php
@@ -11,26 +11,15 @@
     <?php
     include 'included/header.html';
      ?>
-    <div id = "slider">
-        <ul class = "slides">
-            <li class = "slide">
-                <img src="slideshow/slide1.png"/>
-            </li>
-            <li class = "slide">
-                <img src="slideshow/slide2.png"/>
-            </li>
-            <li class = "slide">
-                <img src="slideshow/slide3.png"/>
-            </li>
-            <li class = "slide">
-                <img src="slideshow/slide4.png"/>
-            </li>
-            <li class = "slide">
-                <img src="slideshow/slide1.png"/>
-            </li>
-        </ul>
-    </div>
-    
+   <div id="slideshow">
+    <figure>
+     <img src="slideshow/slide1.png">
+     <img src="slideshow/slide2.png">
+     <img src="slideshow/slide3.png">
+     <img src="slideshow/slide4.png">
+     <img src="slideshow/slide1.png">
+    </figure>
+</div>
     <?php
     include 'included/footer.html';
      ?>
