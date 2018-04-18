4bbd7f981ddf0bf1a57c0c3247560b744ba9711a
Buzykina,Yevheniia Y.V <y.buzykina@student.fontys.nl>
2018-04-18 12:20:48 +0200

    Updated index page

--- Website/index.php
+++ Website/index.php
@@ -7,9 +7,7 @@
     <link rel="stylesheet" type="text/css" href="css/index_css.css">
     <script src="js/index_js.js"></script>
     <link rel="stylesheet" href="css/flipclock.css">
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-
+	<script src="js/aos.js"></script	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="js/flipclock.js"></script>
     </head> 
  <body>
@@ -25,6 +23,40 @@
      <img src="slideshow/slide1.png">
     </figure>
     <div id="clock"></div>
+    <a class="scroll-link" href="#content">
+		<svg class="mouse" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 76 130" preserveAspectRatio="xMidYmid meet">
+		<g fill="none" fill-rule="evenodd">
+			<rect width="70" height="118" x="1.5" y="1.5" stroke="#FFF" stroke-width="3" rx="36"/>
+			<circle class="scroll" cx="36.5" cy="31.5" r="4.5" fill="#FFF"/>
+		</g>
+	</svg>
+	</a>
+</div>
+<div id = "content" class="content">
+    <div>
+    	<img src="img/line_up.jpg" id="lineup">
+    </div>
+    <div>
+        <label>dshdhsrthrthxdfdrdthjngdjgfnrufg</label>
+    </div>
+    <div>
+        <label>dshdhsrthrthxdfdrdthjngdjgfnrufg</label>
+    </div>
+    <div>
+    	<img src="img/experience.jpg" id="experience">
+    </div>
+    <div>
+        <img src="img/feelthisthrill.jpg" id = "ticket">
+    </div>
+    <div>
+        <label>dshdhsrthrthxdfdrdthjngdjgfnrufg</label>
+    </div>
+    <div>
+        <label>dshdhsrthrthxdfdrdthjngdjgfnrufg</label>
+    </div>
+    <div>
+    	<img src="img/gifts.jpg" id="gifts">
+    </div>
 </div>
     <?php
     include 'included/footer.html';
