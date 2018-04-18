9e5e3c6649dcd05fc81d8228f974e43630d18710
Luong,Dung D.T <d.luong@student.fontys.nl>
2018-04-18 19:40:56 +0200

    improve responsiveness

--- Website/experience.php
+++ Website/experience.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="css/Experience.css">
     <?php
     include 'included/common_head.html';
     ?>
@@ -12,9 +13,42 @@
     include 'included/header.html';
      ?>
 
-    <?php
-    include 'included/experience.html';
-    ?>
+    <div class="maincover">
+        <div class="maincover-text">
+            <span class="border1">
+                EXPERIENCE THE THRILL LIKE NEVER BEFORE
+            </span>
+            <br/>
+            <span class="border2">
+                Emerge yourself into the grand adventure of seeking the true essence of music
+            </span>
+        </div>
+        <div class="maincontent">
+            <div class="video">
+                <a href="videopage.php">
+                    <img src="img/video.jpg">
+                    <h3>
+                        Videos
+                    </h3>
+                    <p>
+                        Grab a cup of tea and enjoy our after movies!
+                    </p>
+                </a>
+            </div>
+
+            <div class="pictures">
+                <a href="picturepage.php">
+                    <img src="img/picture.jpg">
+                    <h3>
+                        Pictures
+                    </h3>
+                    <p>
+                        Relive the thrill of previous events!
+                    </p>
+                </a>
+            </div>
+        </div>
+    </div>
 
 
 
