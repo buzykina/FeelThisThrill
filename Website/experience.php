6b99d3d93d12f044b60808de242e7d0477a0e28c
Luong,Dung D.T <d.luong@student.fontys.nl>
2018-04-18 02:34:23 +0200

    php files for 3 pages

---
+++ Website/experience.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <?php
+    include 'included/common_head.html';
+    ?>
+</head>
+    
+    
+ <body>
+    <?php
+    include 'included/header.html';
+     ?>
+
+    <?php
+    include 'included/experience.html';
+    ?>
+
+
+
+    <?php
+    include 'included/footer.html';
+     ?>
+ </body>
+    
+
+</html>
---
+++ Website/picturepage.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <?php
+    include 'included/common_head.html';
+    ?>
+</head>
+
+
+<body>
+<?php
+include 'included/header.html';
+?>
+
+<?php
+include 'included/picturepage.html';
+?>
+
+
+
+<?php
+include 'included/footer.html';
+?>
+</body>
+
+
+</html>
